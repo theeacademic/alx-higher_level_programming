d852deaddb23be74aa2415367dcaaac0539a8d5a
theeacademic <nyawira22.kelly@student.cuk.ac.ke>
2023-11-27 03:48:46 -0800

    Create a new sentence

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print(str[str.find("object-oriented programming"):].split()[0] + " with programming Python")
