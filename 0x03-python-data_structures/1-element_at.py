63a499cb675464503b81e1ebc41c18d0624ea34e
theeacademic <nyawira22.kelly@student.cuk.ac.ke>
2023-12-05 09:39:52 -0800

    done

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,20 +1,11 @@
 #!/usr/bin/python3
+# File: 1-element_at.py
+
 def element_at(my_list, idx):
-    # Check if idx is negative
-    if idx < 0:
+    # Check if idx is negative or out of range
+    if idx < 0 or idx >= len(my_list):
         return None
-
-    # Check if idx is out of range
-    if idx >= len(my_list):
-        return None
-
+    
     # Return the element at the specified index
     return my_list[idx]
-
-# Example usage:
-my_list = [1, 2, 3, 4, 5]
-index_to_retrieve = 2
-
-result = element_at(my_list, index_to_retrieve)
-print(result)
 
