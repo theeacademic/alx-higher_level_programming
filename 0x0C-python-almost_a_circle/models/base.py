93ea755eb83106f0061ed176737acd5329416900
theeacademic <nyawira22.kelly@student.cuk.ac.ke>
2024-01-19 04:05:43 -0800

    Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,16 +2,26 @@
 # models/base.py
 
 class Base:
-    # private class attribute
+    # private class attribute to keep track of instances
     __nb_objects = 0
 
     def __init__(self, id=None):
-        # class constructor
+        """
+        Class constructor for Base.
+
+        Args:
+            id (int): Optional parameter for the instance id.
+                      If provided, sets the public instance attribute 'id' to the given value.
+                      If not provided, increments __nb_objects and sets 'id' to the new value.
+
+        Attributes:
+            id (int): Public instance attribute representing the unique identifier for each instance.
+        """
         if id is not None:
-            # assign the public instance attribute id with the argument value
+            # assign the public instance attribute 'id' with the argument value
             self.id = id
         else:
-            # increment __nb_objects and assign the new value to the public instance attribute id
+            # increment __nb_objects and assign the new value to the public instance attribute 'id'
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
