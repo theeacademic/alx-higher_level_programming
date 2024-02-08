7106970353659581b2730c3104d8a16f3cebfbf7
theeacademic <nyawira22.kelly@student.cuk.ac.ke>
2024-02-08 07:42:40 -0800

    safe int

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -6,13 +6,4 @@ def safe_print_integer(value):
     except (ValueError, TypeError):
         return False
 
-# Test cases
-result1 = safe_print_integer(89)
-result2 = safe_print_integer(-89)
-result3 = safe_print_integer("School is not an integer")
-
-# Output exactly as requested
-print(result1)
-print(result2)
-print(result3)
 
