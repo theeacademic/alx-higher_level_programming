7f8cfba0f0bd92747bc9887ced325e428f24b6d9
theeacademic <nyawira22.kelly@student.cuk.ac.ke>
2024-02-09 01:12:42 -0800

    My first square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
 class Square:
-    """An empty class that defines a square."""
+    """
+    A simple class representing a square.
+
+    Attributes:
+        None
+
+    Methods:
+        None
+
+    Usage:
+        my_square = Square()
+    """
     pass
+
+if __name__ == "__main__":
+    my_square = Square()
+    print(type(my_square))  # Output: <class '0-square.Square'>
+    print(vars(my_square))  # Output: {}
 
