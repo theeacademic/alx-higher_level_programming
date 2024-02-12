eed7a7f61b535638f77c7ee8f8113ecd19274450
theeacademic <nyawira22.kelly@student.cuk.ac.ke>
2024-02-12 03:42:15 -0800

    Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+class Square:
+    """
+    Represents a square shape.
+    """
+    def __init__(self, size=0):
+        """
+        Initializes a Square instance with the given size.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        Retrieves the size of the square.
+
+        Returns:
+            int: The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Sets the size of the square.
+
+        Args:
+            value (int): The new size value.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Calculates and returns the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
+
