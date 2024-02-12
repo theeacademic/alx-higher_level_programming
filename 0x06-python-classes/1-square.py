0b645cded03f686c8060b6eb6f9bd356e9d64a0b
theeacademic <nyawira22.kelly@student.cuk.ac.ke>
2024-02-12 01:20:33 -0800

    object

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,9 +3,6 @@
 class Square:
     """
     A class representing a square.
-
-    Attributes:
-        __size (int): The size of the square (private attribute).
     """
 
     def __init__(self, size):
