86760c309bc8fd6ee888ad0658cc6ce9479cbb22
theeacademic <nyawira22.kelly@student.cuk.ac.ke>
2024-02-12 01:15:07 -0800

    Object

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,20 +1,11 @@
 #!/usr/bin/python3
+"""Define a class Square."""
 class Square:
     """
     A class representing a square.
 
     Attributes:
         __size (int): The size of the square (private attribute).
-
-    Methods:
-        None
-
-    Usage:
-        my_square = Square(3)
-        print(type(my_square))  # Output: <class '1-square.Square'>
-        print(vars(my_square))  # Output: {'_Square__size': 3}
-        print(my_square.size)   # Output: AttributeError: 'Square' object has no attribute 'size'
-        print(my_square.__size) # Output: AttributeError: 'Square' object has no attribute '__size'
     """
 
     def __init__(self, size):
@@ -27,17 +18,4 @@ class Square:
         Note:
             The size is a private attribute.
         """
-        self.__size = size
-
-if __name__ == "__main__":
-    my_square = Square(3)
-    print(type(my_square))  # Output: <class '1-square.Square'>
-    print(vars(my_square))  # Output: {'_Square__size': 3}
-
-    # Attempt to access the private attribute directly
-    try:
-        print(my_square.size)   # Output: AttributeError: 'Square' object has no attribute 'size'
-        print(my_square.__size) # Output: AttributeError: 'Square' object has no attribute '__size'
-    except AttributeError as e:
-        print(e)
 
