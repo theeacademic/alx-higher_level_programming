1838a2694a78be1fcf57a3ce924b5573152ee193
theeacademic <nyawira22.kelly@student.cuk.ac.ke>
2024-02-12 00:37:35 -0800

    'sq

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,21 +1,12 @@
-#!/usr/bin/python3
 class Square:
     """
-    A simple class representing a square.
+    Represents a square shape.
 
     Attributes:
         None
 
     Methods:
         None
-
-    Usage:
-        my_square = Square()
     """
     pass
-
-if __name__ == "__main__":
-    my_square = Square()
-    print(type(my_square))  # Output: <class '0-square.Square'>
-    print(vars(my_square))  # Output: {}
 
